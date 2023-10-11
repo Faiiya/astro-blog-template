ce810e52c712f4eecc9c1ddeffa66ca51d91c5f6
Faiiya <sr.grima@hotmail.com>
2023-10-11 15:12:05 +0200

    prueba optimize custom comps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx({optimize: true}), sitemap(), tailwind()],
+  integrations: [
+    mdx({
+      optimize: { customComponentNames: ["Cta", "CtaBot", "GifSeo", "Gmaps", "Video"] },
+    }),
+    sitemap(),
+    tailwind(),
+  ],
   markdown: {
     rehypePlugins: [rehypeHeadingIds],
   },
