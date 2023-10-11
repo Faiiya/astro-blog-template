9f50a3ef9d3c1f1639055f7d3e557cd9de9e09a7
Faiiya <sr.grima@hotmail.com>
2023-10-11 14:45:13 +0200

    ?

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,16 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		summary: z.string().optional(),
-		date: z.coerce.date().optional(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    summary: z.string().nullable().default("") ,
+    date: z.coerce.date().optional(),
+    banner: z.string(),
+    fondoBanner: z.string(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
